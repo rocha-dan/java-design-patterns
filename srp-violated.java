9aa386724870a177834631d35759978ebde8194c
Daniel Rocha <daniel_rocha2009@hotmail.com>
2023-01-30 15:14:23 -0300

    Add files via upload

---
+++ srp-violated.java
@@ -0,0 +1,5 @@
+public class Funcionario {
+  public Pagamento calcularPagamento() {}
+  public void save() {}
+  public String detalhesFuncionario() {}
+}
---
+++ srp.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+  public String detalhesFuncionario() {}
+}
+//------
+public class Pagamentos {
+  public String calcularPagamento() {}
+}
+//------
+public class FuncionarioRepositorio {
+  public String save() {}
+}
