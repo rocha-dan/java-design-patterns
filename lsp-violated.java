f1ca05040c3d55c3b6971ab59825a58ad6fe7fd0
Daniel Rocha <daniel_rocha2009@hotmail.com>
2023-01-31 10:44:23 -0300

    Add files via upload

---
+++ lsp-violated.java
@@ -0,0 +1,9 @@
+public class Automovel{
+    public void ligarMotor(){
+      ...
+
+    }
+}
+public class Bicicleta extends Automovel{
+  
+}
---
+++ lsp.java
@@ -0,0 +1,21 @@
+public class AutomovelEletrico{
+    public void ligarMotor(){
+      ...
+
+    }
+}
+
+public class AutomovelManual{
+    public void movimentar(){
+      ...
+
+    }
+}
+//----
+public class Bicicleta extends AutomovelManual{
+  
+}
+
+public class Motocicleta extends AutomovelEletrico{
+  
+}
